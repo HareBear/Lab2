374d6c200370c3c5461d728c75692ab339381e64
Harrison <harrison.milbradt@gmail.com>
2018-01-20 11:51:17 -0800

    Displaying additional actor data, reformated actor data for clarity, updated changelog
    Signed-off-by: Harrison <harrison.milbradt@gmail.com>

--- application/views/actor.php
+++ application/views/actor.php
@@ -1,7 +1,9 @@
 <div class="row">
-        <div class="span4">
-                <a href="/welcome/show/{key}">
-                        <img src="/pix/{who}.jpg" title="{who} plays {role}"/>
-                </a>
-        </div>
+    <div class="span4">
+        <h2>{who}</h2>
+        <a href="/welcome/show/{key}">
+            <img src="/pix/{who}.jpg" title="{who} plays {role}"/>
+        </a>
+        <p>{who} plays the role of {role}, where they are qouted saying: <blockquote>"{what}"</blockquote></p>
+    </div>
 </div>
